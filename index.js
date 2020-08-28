b0a5763d99575e5f6be854b5581f749c7a3845f7
Danillo Barreto <danillo.barreto@catho.com>
2020-08-27 22:16:58 -0300

    feat:* create chat application

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  io.emit('connected', 'User has connected');
+  socket.on('disconnect', () => {
+    io.emit('user disconnected', 'User has disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
