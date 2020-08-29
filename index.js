ff02cd181f206d9d59e650f1ee4d293f4cbb57f2
Danillo Barreto <danillo.barreto@catho.com>
2020-08-29 16:01:00 -0300

    feat:* improve is typing feature

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+let usersTypingList = [];
 io.on('connection', (socket) => {
   io.emit('connected', 'User has connected');
   socket.on('disconnect', () => {
@@ -14,9 +15,37 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     socket.broadcast.emit('chat message', msg);
   });
-  socket.on('typing', (msg) => {
-    socket.broadcast.emit('typing', msg);
+  socket.on('typing', (user) => {
+    if (usersTypingList.indexOf(user) == -1) {
+      usersTypingList.push(user);
+    }
+
+    emitUsersTypingIfNeeded(usersTypingList, socket);
   });
+  socket.on('not typing', (user) => {
+    let indexOf = usersTypingList.indexOf(user);
+    if (indexOf == -1) {
+      return;
+    }
+
+    usersTypingList.splice(indexOf, 1);
+
+    emitUsersTypingIfNeeded(usersTypingList, socket);
+  });
+
+  function emitUsersTypingIfNeeded(usersTypingList, socket) {
+    if (usersTypingList.length == 0) {
+      io.emit('typing', '');
+      return;
+    }
+
+    usersTyping = usersTypingList.join(', ');
+    isOrAreTyping = ' is typing...'
+    if (usersTypingList.length > 1) {
+      isOrAreTyping = ' are typing...';
+    }
+    io.emit('typing', usersTyping + isOrAreTyping);
+  }
 });
 
 http.listen(3000, () => {
